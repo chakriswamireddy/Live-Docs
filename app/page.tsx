e19c5b28baf550c43b0842e1a93dd0a22778aba3
chakriswamireddy <iamthechakri@gmail.com>
2024-09-07 19:30:58 +0530

    resolved routing error

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 // app/page.tsx
-import { redirect,useRouter } from 'next/navigation';
-// import { useRouter } from 'next/router';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  // if (! [ 'linkedin' ,'github' ,'mail'].some(substring => router._.includes(substring)) ) {
-
-    
-
-    redirect('/documents');
-  // }
+  redirect('/documents');
   return null; // No need to render anything
 }
